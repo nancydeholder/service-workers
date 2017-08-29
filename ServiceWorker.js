1006b2e656cfc2675571f708342982fb28053c01
nancydeholder <nancydeholder@gmail.com>
2017-08-29 10:41:14 -0500

    fixing broken inks

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -2,10 +2,10 @@ self.addEventListener('install', function(event) {
 	event.waitUntil(
 		caches.open('v1').then(function(cache) {
 			return cache.addAll([
-				'/index.html',
-				'/style.css',
-				'/doggie.jpg',
-				'/demo.js'
+				'index.html',
+				'style.css',
+				'doggie.jpg',
+				'demo.js'
 			]);
 		})
 	);
