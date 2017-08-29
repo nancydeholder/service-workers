6cc393d7ebeb544106342135a3046aba02df0feb
nancydeholder <nancydeholder@gmail.com>
2017-08-28 21:45:33 -0500

    customizing service worker

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -3,6 +3,8 @@ self.addEventListener('install', function(event) {
 		caches.open('v1').then(function(cache) {
 			return cache.addAll([
 				'index.html',
+				'doggie.png',
+				'style.css',
 				'demo.js'
 			]);
 		})
