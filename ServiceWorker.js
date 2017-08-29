0e0d864e9e7ca22b05307428cdca12d466a8c008
nancydeholder <nancydeholder@gmail.com>
2017-08-29 08:43:44 -0500

    Making the cache work

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-this.addEventListener('install', function(event) {
+self.addEventListener('install', function(event) {
 	event.waitUntil(
 		caches.open('v1').then(function(cache) {
 			return cache.addAll([
@@ -9,10 +9,26 @@ this.addEventListener('install', function(event) {
 	);
 });
 
-this.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch', function(event) {
+  event.respondWith(caches.match(event.request).then(function(response) {
+    // caches.match() always resolves
+    // but in case of success response will have value
+    if (response !== undefined) {
+      return response;
+    } else {
+      return fetch(event.request).then(function (response) {
+        // response may be used only once
+        // we need to save clone to put one copy in cache
+        // and serve second one
+        let responseClone = response.clone();
+        
+        caches.open('v1').then(function (cache) {
+          cache.put(event.request, responseClone);
+        });
+        return response;
+      }).catch(function () {
+        console.log('returning catch');
+      });
+    }
+  }));
 });
